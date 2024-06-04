eb409b92c7af37dbc8bc9a1940c8becce6e7235d
MogammedRa-Eez <zardad2603@gmail.com>
2024-06-04 00:09:49 -0700

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,33 +1,52 @@
 #!/usr/bin/python3
 """
-A script that starts a Flask web application:
+Return list of available states either from file or DB storage
 """
-
-from flask import Flask
 from models import storage
-from flask import render_template
+from flask import Flask, render_template
+from models.state import State
+import models
+
+
+class StateInfo:
+    """
+    Interface that template receives regardless of storage type
+    """
+    def __init__(self, id, name):
+        """
+        State ID and name
+        """
+        self.id = id
+        self.name = name
+
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list_route():
-    """
-    List states: display a HTML page: (inside the tag BODY)
-    Returns:
-        html: template that lists all states sort by name A->Z
-    """
-    states = storage.all("State").values()
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def close_db(exception=None):
+def close_storage(exception):
     """
-    After each request remove the current SQLAlchemy Session:
+    Close file/DB storage on app teardown
     """
     storage.close()
 
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """
+    Displays all states
+    """
+    states = []
+    if models.storage_type == "db":
+        results = storage.all("State").values()
+    else:
+        results = storage.all(State).values()
+    for result in results:
+        new_state = StateInfo(result.id, result.name)
+        states.append(new_state)
+    states.sort(key=lambda s: s.name)
+    return render_template("7-states_list.html",
+                           states=states)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
